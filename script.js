e83edf18b53807bb5457442528c5b02719d357ae
Jake Jackson <142851548+JakebJackson@users.noreply.github.com>
2023-12-13 14:02:52 +1100

    Completed functionality for password generator

--- script.js
+++ script.js
@@ -1,11 +1,57 @@
-var passwordLength;
-var incUpperCase = false;
-var incLowerCase = false;
-var incNumbers = false;
-var incSpecialChar = false;
-
 // Function for generating the password, called on button click by user later in code.
 function generatePassword() {
+  var passwordLength;
+  var random;
+  var charCode;
+  var password = "";
+  var charactersLength = 0;
+  var paramAmount = 0;
+  var incUpperCase = false;
+  var incLowerCase = false;
+  var incNumbers = false;
+  var incSpecialChar = false;
+  var lowerCase = [];
+  var upperCase = [];
+  var numbers = [];
+  var specialChar = [];
+  var usedCharacters = [];
+
+  // For loop to add all lower case letters to an array using CharCode.
+  for (i = 97; i < 123; i++) {
+    charCode = String.fromCharCode(i);
+    lowerCase.push(charCode);
+  }
+
+  // For loop to add all upper case letters to an array using CharCode.
+  for (i = 65; i < 91; i++) {
+    charCode = String.fromCharCode(i);
+    upperCase.push(charCode);
+  }
+
+  // For loop to add all numbers letters to an array using CharCode.
+  for (i = 48; i < 58; i++) {
+    charCode = String.fromCharCode(i);
+    numbers.push(charCode);
+  }
+
+  // The following for loops are to add all special characters to an array using CharCode, I used four because they unicodes are split.
+  for (i = 32; i < 48; i++) {
+    charCode = String.fromCharCode(i);
+    specialChar.push(charCode);
+  }
+  for (i = 58; i < 65; i++) {
+    charCode = String.fromCharCode(i);
+    specialChar.push(charCode);
+  }
+  for (i = 91; i < 97; i++) {
+    charCode = String.fromCharCode(i);
+    specialChar.push(charCode);
+  }
+  for (i = 123; i < 127; i++) {
+    charCode = String.fromCharCode(i);
+    specialChar.push(charCode);
+  }
+
   // Asking user for desired PW length and storing their input in a variable.
   passwordLength = prompt("How many characters would you like your password to be? Choose a number between 8 and 128.");
 
@@ -50,9 +96,40 @@ function generatePassword() {
   // Logs when the (while) loop has been broken.
   console.log("Parameters chosen.");
 
-  // Need to check what parameters are being used
-  // Need to check how long the password will be
-  // Need to randomly select what the next character will be and store it
+  // Logs parameter amount.
+  console.log("Amount of parameters being used: " + paramAmount)
+
+  // The following if statements check to see which parameters are true and adds the length of the array to a variable called charactersLength and also adds the array itself to a seperate array called usedCharacters.
+  if (incUpperCase) {
+    usedCharacters = usedCharacters.concat(upperCase);
+  }
+  if (incLowerCase) {
+    usedCharacters = usedCharacters.concat(lowerCase);
+  }
+  if (incNumbers) {
+    usedCharacters = usedCharacters.concat(numbers);
+  }
+  if (incSpecialChar) {
+    usedCharacters = usedCharacters.concat(specialChar);
+  }
+
+  charactersLength = usedCharacters.length;
+  console.log(usedCharacters);
+
+  function randomCharacter() {
+    random = Math.floor(Math.random() * charactersLength);
+    console.log("Random number is: " + random);
+
+    return usedCharacters[random];
+  }
+
+  // Function for generating the password, will keep generating a random character and adding it to password until the password is as long as the user specified.
+  for (var i = 0; i <= passwordLength; i++) {
+    password = password + randomCharacter();
+  }
+  console.log(password);
+
+  return password;
 }
 
 // Assignment Code
